30a349955d3797a3d2a0e9bd4fd0ff4be2f9f569
khadija <khadijalahmar8@gmail.com>
2022-01-06 22:56:03 +0100

    connection basse donner

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express =require('express');
+const path =require('path');
+const mysql = require('mysql2');
 const app = express();
 
+
+// database
+const database=require('./config/database')
+
+database.authenticate()
+.then(()=>console.log('Database connect'))
+.catch(err => console.log('Error : '+err))
+
+
+
+
+app.get('/',(req ,res) =>
+res.send('hello'));
 
 
 

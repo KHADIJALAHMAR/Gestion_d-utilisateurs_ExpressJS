67ae0a861c670e147e82e6aec3c6b11d7b32b1ac
khadija <khadijalahmar8@gmail.com>
2022-01-10 11:49:41 +0100

    create fil userController.js

--- routes/route.js
+++ routes/route.js
@@ -3,6 +3,10 @@ const router =express.Router();
 const database =require('../config/database');
 
 
+
+
+
+
 router.get("/all",(req,res) =>{
     database.Department.findAll()
     .then(Deparetements => res.send(Deparetements))
